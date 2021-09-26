d78ca69627355dcc2c7f6d404139a8f5f5fa55b2
Elena <enafukin@gmail.com>
2021-09-26 20:58:34 +0400

    Добавляет стилизацию блока табов для desktop

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,19 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
-
+const tabsNav = document.querySelector('.tabs__nav');
+const tabs = document.querySelector('.tabs__list');
+/*
+const Ways = {
+  GREECE: 'greece',
+  ALBANIA: 'albania',
+  MACEDONIA: 'macedonia',
+  MONTENEGRO: 'montenegro',
+  CROATIA: 'croatia',
+};
+*/
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
@@ -12,3 +22,19 @@ navToggle.addEventListener('click', function() {
     navMain.classList.add('main-nav--closed');
   }
 });
+
+const getTabHandler = (evt) => {
+  evt.preventDefault();
+  tabsNav.querySelectorAll('.tabs__link').forEach((item) => item.classList.remove('tabs__link--active'));
+  tabs.querySelectorAll('.tab').forEach((tab) => {
+    tab.classList.add('tab--hidden');
+
+    if (tab.id === `#${evt.target.dataset.way}`) {
+      tab.classList.remove('tab--hidden');
+      evt.target.classList.add('tabs__link--active');
+    }
+  });
+
+}
+
+tabsNav.addEventListener('click', getTabHandler);
